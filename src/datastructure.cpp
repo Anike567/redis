a69032c87e4ec55291eb0531c824026e947e1ad4
Apple <apple@Apples-MacBook-Air.local>
2025-11-03 14:32:23 +0530

    working on bst

---
+++ src/datastructure.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<map>
+
+
+using namespace std;
+
+
+class 
+
+int main(void){
+
+    // map<string, Template<
+    // cout<<"hello wordld";
+
+    map<string, redisValue> mp;
+
+    mp["names"] = vector<int>(10, -1);
+    cout<<myMin(10, 20);
+    return 0;
+}
